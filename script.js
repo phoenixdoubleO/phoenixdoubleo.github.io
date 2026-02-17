fac50f4a6abffac16bfe9515b9adcf7cb9b6cc80
phoenixstar <107529995+phoenixdoubleO@users.noreply.github.com>
2026-02-16 23:38:50 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-// Grab the button and sidebar
 const toggleButton = document.getElementById("menuToggle");
 const sidebar = document.querySelector(".sidebar");
 
-// On click, toggle the "closed" class
 toggleButton.addEventListener("click", () => {
   sidebar.classList.toggle("closed");
 });
